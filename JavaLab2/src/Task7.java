cc094e90d9d1abea245557b926131cf583c180e0
unknown <var1541b@k3-426-8.edu.ugrasu>
2016-02-09 10:07:59 +0500

    Исправил 7

--- JavaLab2/src/Task7.java
+++ JavaLab2/src/Task7.java
@@ -1,6 +1,5 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
@@ -14,27 +13,36 @@ import java.io.InputStreamReader;
  */
 public class Task7 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Введите X и Y");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String a_str = in.readLine(); //читаем строку с клавиатуры
-        String b_str = in.readLine();
+        int a = 0;
+        int b = 0;
+        try {
+            String a_str = in.readLine(); //читаем строку с клавиатуры
+            a = Integer.parseInt(a_str); //преобразовываем строку в число.
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода 1 числа");
+        }
 
-        int a = Integer.parseInt(a_str); //преобразовываем строку в число.
-        int b = Integer.parseInt(b_str);
+        try {
+            String b_str = in.readLine(); //читаем строку с клавиатуры
+            b = Integer.parseInt(b_str); //преобразовываем строку в число.
+        } catch (Exception e) {
+            System.out.println("Ошибка ввода 2 числа");
 
-        if ((a > 0) && (b > 0)) {
-            System.out.println("1 четверть");
-        }
-        if ((a < 0) && (b > 0)) {
-            System.out.println("2 четверть");
-        }
-        if ((a < 0) && (b < 0)) {
-            System.out.println("3 четверть");
-        }
-        if ((a > 0) && (b < 0)) {
-            System.out.println("4 четверть");
+            if ((a > 0) && (b > 0)) {
+                System.out.println("1 четверть");
+            } else if ((a < 0) && (b > 0)) {
+                System.out.println("2 четверть");
+            } else if ((a < 0) && (b < 0)) {
+                System.out.println("3 четверть");
+            } else if ((a > 0) && (b < 0)) {
+                System.out.println("4 четверть");
+            } else {
+                System.out.println("Точка лежит на пересечении четвертей");
+            }ЫЫ
         }
     }
 }
